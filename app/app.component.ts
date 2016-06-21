b3c5f958760e8d3cb87ed7e2259b62df4c375fb6
zach larson <zlarson@gmail.com>
2016-06-21 09:54:20 -0500

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+  <h3>Angular2 is Working!</h3>
+  `
+})
+export class AppComponent {
+  constructor() { }
+}
---
+++ app/bootstrap.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
